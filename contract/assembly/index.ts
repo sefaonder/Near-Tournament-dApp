579bd9780c1418ad946aa1bcd29460ca78634774
Sefa Önder <sefa.onder@oymakyazilim.com>
2022-04-24 23:27:11 +0300

    contract finished

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,34 +1,131 @@
-/*
- * This is an example of an AssemblyScript smart contract with two simple,
- * symmetric functions:
- *
- * 1. setGreeting: accepts a greeting, such as "howdy", and records it for the
- *    user (account_id) who sent the request
- * 2. getGreeting: accepts an account_id and returns the greeting saved for it,
- *    defaulting to "Hello"
- *
- * Learn more about writing NEAR smart contracts with AssemblyScript:
- * https://docs.near.org/docs/develop/contracts/as/intro
- *
- */
+import { Context, ContractPromiseBatch, logging, storage, u128 } from "near-sdk-as";
+import { tournaments, Tournament, Tournament } from "./model";
 
-import { Context, logging, storage } from 'near-sdk-as'
+export const ONE_NEAR_YOKTO = u128.from("1000000000000000000000000");
 
-const DEFAULT_MESSAGE = 'Hello'
-
-// Exported functions will be part of the public interface for your smart contract.
-// Feel free to extract behavior to non-exported functions!
-export function getGreeting(accountId: string): string | null {
-  // This uses raw `storage.get`, a low-level way to interact with on-chain
-  // storage for simple contracts.
-  // If you have something more complex, check out persistent collections:
-  // https://docs.near.org/docs/concepts/data-storage#assemblyscript-collection-types
-  return storage.get<string>(accountId, DEFAULT_MESSAGE)
+export function toNear(deposit: u128): u32 {
+  return u128.div(deposit, ONE_NEAR_YOKTO).toU32();
 }
 
-export function setGreeting(message: string): void {
-  const accountId = Context.sender
-  // Use logging.log to record logs permanently to the blockchain!
-  logging.log(`Saving greeting "${message}" for account "${accountId}"`)
-  storage.set(accountId, message)
+export function toYocto(amount: number): u128 {
+  return u128.mul(ONE_NEAR_YOKTO, u128.from(amount));
+}
+
+/* Call Methods */
+
+export function createTournament(description: string, imageUrl: string, name: string, tournamentFee: u16): string {
+  //create a new tournament
+  const Tournament = new Tournament(description, tournamentFee, name, imageUrl);
+
+  //set the tournament in storage
+  tournaments.set(Tournament.id, Tournament);
+  return "Succesfully Created";
+}
+
+export function updateTournamentContent(TournamentID: u32, description: string, tournamentFee: u16, name: string, imageUrl: string): string {
+  assertTournament(TournamentID);
+  const Tournament = tournaments.getSome(TournamentID);
+
+  //assign statements
+  Tournament.description = description;
+  Tournament.tournamentFee = tournamentFee;
+  Tournament.imgUrl = imageUrl;
+  Tournament.name = name;
+
+  tournaments.set(TournamentID, Tournament);
+  return "Succesfully Updated";
+}
+
+//Delete a existing Tournament
+export function deleteTournament(TournamentID: u32): string {
+  assertTournament(TournamentID);
+  //delete the tournaments in storage
+  tournaments.delete(TournamentID);
+  return "Succesfully Deleted";
+}
+
+export function applyTournament(TournamentID: u32): string {
+  assertTournament(TournamentID);
+
+  const user = Context.predecessor;
+
+  //get Tournament
+  const Tournament = tournaments.getSome(TournamentID);
+
+  //assert statements
+  assert(Tournament.publisher != user, "You cannot apply your own tournament");
+  assert(!Tournament.applicants.includes(user), "Cannot reapply tournament");
+
+  //deposit Tournament fee
+  const deposit = Context.attachedDeposit;
+  assert(toNear(deposit) == Tournament.tournamentFee, `Tournament Fee is ${Tournament.tournamentFee}`);
+  Tournament.applicants.push(user);
+
+  //set the appliars in storage
+  tournaments.set(TournamentID, Tournament);
+  return "Succesfully applied";
+}
+
+//remove apply
+export function removeApply(TournamentID: u32): string {
+  assertTournament(TournamentID);
+  const user = Context.predecessor;
+  const Tournament = tournaments.getSome(TournamentID);
+
+  assert(Tournament.applicants.includes(user), "You already didn't apply this tournament");
+  const index = Tournament.applicants.indexOf(user);
+  Tournament.applicants.splice(index, 1);
+
+  tournaments.set(TournamentID, Tournament);
+  //send money back to appliear
+  const appliear = ContractPromiseBatch.create(user);
+  const promise = appliear.transfer(toYocto(Tournament.tournamentFee));
+
+  return "Succesfully remove apply";
+}
+
+export function voteTournament(TournamentID: u32, isVoteUp: bool): string {
+  assertTournament(TournamentID);
+
+  const Tournament = tournaments.getSome(TournamentID);
+  //vote Tournament and set the storage
+  if (isVoteUp) {
+    Tournament.vote = Tournament.vote + 1;
+  } else {
+    Tournament.vote = Tournament.vote - 1;
+  }
+  tournaments.set(TournamentID, Tournament);
+  return "Succesfully voted";
+}
+
+/* View Methods */
+
+export function getTournaments(): Tournament[] {
+  //compare function to compare vote field
+  function compare(a: Tournament, b: Tournament): i16 {
+    if (a.vote < b.vote) {
+      return 1;
+    }
+    if (a.vote > b.vote) {
+      return -1;
+    }
+    return 0;
+  }
+
+  const arr = tournaments.values(0);
+  // The array is sorted according to the votes
+  arr.sort(compare);
+  return arr.slice(0, 10);
+}
+
+export function getTournament(TournamentID: u32): Tournament {
+  assertTournament(TournamentID);
+  const Tournament = tournaments.getSome(TournamentID);
+  return Tournament;
+}
+
+//assert Tournament for check Tournament is exists
+function assertTournament(TournamentID: u32): void {
+  const Tournament = tournaments.get(TournamentID);
+  assert(Tournament, "There is no such a tournament");
 }
---
+++ contract/assembly/model.ts
@@ -0,0 +1,33 @@
+import { PersistentUnorderedMap, math, u128, base64, base58, Context, PersistentMap, PersistentSet } from "near-sdk-as";
+
+export const tournaments = new PersistentUnorderedMap<u32, Tournament>("t");
+
+// check this out for generateUniqueID function
+// https://stackoverflow.com/questions/69684748/how-to-create-a-uid-in-assemblyscript-for-a-near-contract
+
+export function generateUniqueID(): u32 {
+  const title = Context.sender.substring(0, Context.sender.lastIndexOf("."));
+  return math.hash32(title + "-" + Context.blockIndex.toString());
+}
+
+@nearBindgen
+export class Tournament {
+  id: u32; //unique id for tournaments
+  publisher: string; //tournament publisher
+  description: string; //tournament description
+  name: string; // tournament name
+  imgUrl: string; //image url
+  tournamentFee: u32; //tournament entry fee
+  vote: i8; // voting down or up for users
+  applicants: Array<string> = [];
+
+  constructor(description: string, tournamentFee: u16, name: string, imageUrl: string) {
+    this.id = generateUniqueID();
+    this.publisher = Context.sender;
+    this.name = name;
+    this.description = description;
+    this.tournamentFee = tournamentFee;
+    this.imgUrl = imageUrl;
+    this.vote = 0;
+  }
+}
